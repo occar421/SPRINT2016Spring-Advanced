07fbab77c071c656b7b738efd25a89ab3a1f4d96
MasuqaT <occar@hotmail.co.jp>
2016-06-03 01:05:52 +0900

    Implement ping command and make code more easy-mentenance

---
+++ Webocket/src/Webocket/Session.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Webocket
+{
+	public class Session
+	{
+		private WeakReference<WebSocket> socket;
+
+		public int Id { get; }
+
+		public Session(WebSocket socket)
+		{
+			Id = Counter.Instance.Increment();
+			this.socket = new WeakReference<WebSocket>(socket);
+		}
+
+		public async Task Deal()
+		{
+			var buffer = new byte[1024];
+			WebSocket s;
+
+			if (!socket.TryGetTarget(out s))
+			{
+				return;
+			}
+			var received = await s.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+			while (received.MessageType == WebSocketMessageType.Text)
+			{
+				var data = Encoding.UTF8.GetString(buffer, 0, received.Count);
+				var repeatContainer = new ResponseContainer { Data = data, Id = Id };
+				var repeatBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(repeatContainer));
+
+				await Broadcast(repeatBytes);
+
+				if (data.StartsWith("bot "))
+				{
+					var elem = data.Split(' ');
+					switch (elem[1])
+					{
+						case "ping":
+							var pingContainer = new ResponseContainer { Data = "pong", Id = Id };
+							var pingBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(pingContainer));
+							await Broadcast(pingBytes);
+							break;
+						default:
+							break;
+					}
+				}
+
+				if (!socket.TryGetTarget(out s))
+				{
+					return;
+				}
+				received = await s.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+			}
+
+			if (!socket.TryGetTarget(out s))
+			{
+				return;
+			}
+			await s.CloseAsync(received.CloseStatus.Value, received.CloseStatusDescription, CancellationToken.None);
+		}
+
+		private async Task Broadcast(byte[] bytes)
+		{
+			await Task.WhenAll(Startup.Sockets.Select(x =>
+			{
+				WebSocket s;
+				if (!x.socket.TryGetTarget(out s))
+				{
+					return Task.CompletedTask;
+				}
+				return s.SendAsync(new ArraySegment<byte>(bytes, 0, bytes.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+			}));
+		}
+
+		sealed class Counter
+		{
+			private static readonly Lazy<Counter> lazy = new Lazy<Counter>(() => new Counter());
+
+			public static Counter Instance { get { return lazy.Value; } }
+
+			public int Increment()
+			{
+				return Interlocked.Increment(ref value);
+			}
+
+			private Counter() { }
+
+			private int value = 0;
+		}
+	}
+}
--- Webocket/src/Webocket/Startup.cs
+++ Webocket/src/Webocket/Startup.cs
@@ -8,15 +8,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading;
 using System.Net.WebSockets;
-using System.Text;
-using Newtonsoft.Json;
 using System.Collections.Concurrent;
 
 namespace Webocket
 {
 	public class Startup
 	{
-		private ConcurrentDictionary<int, WebSocket> sockets = new ConcurrentDictionary<int, WebSocket>();
+		private static readonly Lazy<ConcurrentBag<Session>> lazy = new Lazy<ConcurrentBag<Session>>(() => new ConcurrentBag<Session>());
+		public static ConcurrentBag<Session> Sockets => lazy.Value;
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
@@ -36,27 +35,11 @@ namespace Webocket
 					{
 						if (webSocket?.State == WebSocketState.Open)
 						{
-							var id = sockets.Any() ? sockets.Keys.Max() + 1 : 0;
-							if (sockets.TryAdd(id, webSocket))
-							{
-								var buffer = new byte[1024];
-								var received = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+							var session = new Session(webSocket);
+							Sockets.Add(session);
 
-								while (received.MessageType == WebSocketMessageType.Text)
-								{
-									var data = Encoding.UTF8.GetString(buffer, 0, received.Count);
-									var container = new ResponseContainer { Data = data, Id = id };
-									var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(container));
-
-									await Broadcast(bytes);
-
-									received = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-								}
-
-								await webSocket.CloseAsync(received.CloseStatus.Value, received.CloseStatusDescription, CancellationToken.None);
-
-								return;
-							}
+							await session.Deal();
+							return;
 						}
 					}
 				}
@@ -65,11 +48,6 @@ namespace Webocket
 
 			app.UseDefaultFiles();
 			app.UseStaticFiles();
-		}
-
-		private async Task Broadcast(byte[] bytes)
-		{
-			await Task.WhenAll(sockets.Where(x => x.Value.State == WebSocketState.Open).Select(x => x.Value.SendAsync(new ArraySegment<byte>(bytes, 0, bytes.Length), WebSocketMessageType.Text, true, CancellationToken.None)));
 		}
 	}
 }
