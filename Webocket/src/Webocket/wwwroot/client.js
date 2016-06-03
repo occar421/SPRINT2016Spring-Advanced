d2121994a4c8da00407ee4a36b4758c92a45c899
MasuqaT <occar@hotmail.co.jp>
2016-06-03 20:21:08 +0900

    Make express newline

--- Webocket/src/Webocket/wwwroot/client.js
+++ Webocket/src/Webocket/wwwroot/client.js
@@ -14,8 +14,9 @@ $(function () {
     return false;
   });
   ws.onmessage = function(msg){
-    var returnObject = JSON.parse(msg.data);
-    $('#messages').append($('<li>')).append($('<span id="clientId" class="' + (returnObject.isBot ? 'bot' : 'user') + '">').text(returnObject.id)).append($('<span id="clientMessage">').text(returnObject.data));
+  	var returnObject = JSON.parse(msg.data);
+  	var content = returnObject.data.split('\n').join('<br />');
+    $('#messages').append($('<li>')).append($('<span id="clientId" class="' + (returnObject.isBot ? 'bot' : 'user') + '">').text(returnObject.id)).append($('<span id="clientMessage">' + content + '<span />'));
   };
   ws.onerror = function(err){
     console.log("err", err);
